6ed1203978d6771483413e5a401c02bb5db1e642
Christina Dong <christinadongla@yahoo.com>
2019-01-11 20:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var newObj = Object.assign({}, object, {key: value});
+  return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
