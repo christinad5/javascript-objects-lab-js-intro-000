3b39cf43ba65cc2c3e60efb67e02621641e6f7b0
Christina Dong <christinadongla@yahoo.com>
2019-01-11 19:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-function destructivelyUpdateObjectWithKeyandValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
